0923433a2707f57fb822852d6df1b00a97efe595
vaishnavipaliya11 <81965405+vaishnavipaliya11@users.noreply.github.com>
2021-09-21 10:21:11 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ export default function App() {
 
   const musicCollection = {
     devotional: [
-      { name: "shri krishna", rating: "4/5" },
-      { name: "om namah shivay", rating: "3.5/5" },
-      { name: "oo palan hare", rating: "3/5" }
+      { name: "shri krishna", description :"this is an krishna song",rating: "4/5" },
+      { name: "om namah shivay", description:"this is an lord shiva song", rating: "3.5/5" },
+      { name: "oo palan hare", description:"this is an devotional song", rating: "3/5" }
     ],
     rock: [
-      { name: "hey jude", rating: "4.8/5" },
-      { name: "all long the watch tower", rating: "3.8/5" },
-      { name: "memories", rating: "2.8/5" }
+      { name: "hey jude", description:"this song is sung by artist the beatles releasd in 1968", rating: "4.8/5" },
+      { name: "all long the watch tower", description:"this song is sung by artist Jimmi Hendrix releasd in 1968", rating: "3.8/5" },
+      { name: "memories",  description:"this song is sung by artist the Maroon5 releasd in 2019",rating: "2.8/5" }
     ],
     instrumental: [
-      { name: "wipe out", rating: "4.5/5" },
-      { name: "apache", rating: "4/5" },
-      { name: "rebel rouser", rating: "4.8/5" }
+      { name: "wipe out",description:"this song is sung by artist the Surfaries releasd in 1988", rating: "4.5/5" },
+      { name: "apache",description:"this song is sung by artist the Shadows,Cliff Richard  releasd in 1960", rating: "4/5" },
+      { name: "rebel rouser",description:"this song is sung by artist the Dunae Eddy releasd in 2011", rating: "4.8/5" }
     ]
   };
 
@@ -45,7 +45,9 @@ export default function App() {
         {musicCollection[musictoShow].map((music) => (
           <li class="list" key={music.name}>
             <div>{music.name}</div>
+            <div>{music.description}</div>
             <div>{music.rating}</div>
+            
           </li>
         ))}
       </div>
